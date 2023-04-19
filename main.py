fed0a58b683d38534a34d98f1f9939a39b36e0c4
Mohamed El-Gabry <mgabry9@gmail.com>
2023-04-19 08:43:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,24 +3,43 @@
 import os
 import discord
 from discord.ext import commands
+import openai
 
+# Initialize the OpenAI client
+openai.api_key = os.environ["OPENAI_TOKEN"]
 
 intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+intents.members = True  # You need to explicitly set this to access members data
 
+# Create a bot instance with the specified intents
+bot = commands.Bot(command_prefix='/', intents=intents)
 
+# Define a function to generate an OpenAI response
+async def generate_response(message):
+    # Use OpenAI to generate a response
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=message.content,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    # Send the response back to the user
+    await message.channel.send(response.choices[0].text)
+
+# Set up a message listener
 @bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user}")
+async def on_message(message):
+    # Ignore messages from the bot itself
+    if message.author == bot.user:
+        return
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+    # Generate a response using OpenAI
+    await generate_response(message)
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send("Choo choo! 🚅")
+
 
 
 bot.run(os.environ["DISCORD_TOKEN"])
